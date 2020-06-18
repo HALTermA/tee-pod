a9f2cacacfef3b6c0c94e49dbf312729d23c2256
Micchili <wot.dannbo@gmail.com>
2020-06-18 10:16:48 +0900

    returnText関数作成

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello world!");
+});
+
+exports.returnText = functions.https.onRequest((request, response) => {
+  if (request.method !== 'POST') {
+    response.status(400).send("GETで送ってるやで") 
+  }
+  if (request.body.date === undefined) {
+    response.status(400).send("データがないやで")
+  }
+  response.send(`date: ${request.body.date}`)
+});
