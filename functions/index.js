a068e3e80cef8ec99d8e97f737a76b07fd40222c
Micchili <wot.dannbo@gmail.com>
2020-06-26 23:04:45 +0900

    returnColor作成

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,25 @@ const functions = require('firebase-functions');
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send(`Hello ${escapeHtml(request.query.name || request.body.name || 'World')}!`);
+exports.returnColor = functions.https.onRequest((request, response) => {
+  let data;
+  let status;
+  const normal = "normal", warning = "warning", partyPeople = "paetyPeople";
+
+  if (request.body.color.colors === normal || warning || partyPeople ) {
+    switch (request.get('content-type')) {
+      case 'application/json':
+        data = request.body;
+        status = 200;
+        break;
+      default:
+        data = "Context-Typeを設定してください";
+        status = 400;
+        break;
+    }
+    response.status(status).send(data);
+  }
+  else {
+    response.status(400).send("JSONの値が違います");
+  }
 });
