d1bd6248d5c43a24a19490ef85dd76553d13c88b
Micchili <wot.dannbo@gmail.com>
2020-06-29 14:00:21 +0900

    Lightを点灯/消灯する関数作成

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,33 @@ const functions = require('firebase-functions');
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.returnColor = functions.https.onRequest((request, response) => {
+  const normal = "normal", warning = "warning", partyPeople = "paetyPeople";
   let data;
   let status;
-  const normal = "normal", warning = "warning", partyPeople = "paetyPeople";
 
   if (request.body.color.colors === normal || warning || partyPeople ) {
+    switch (request.get('content-type')) {
+      case 'application/json':
+        data = request.body;
+        status = 200;
+        break;
+      default:
+        data = "Context-Typeを設定してください";
+        status = 400;
+        break;
+    }
+    response.status(status).send(data);
+  }
+  else {
+    response.status(400).send("JSONの値が違います");
+  }
+});
+
+exports.isLight = functions.https.onRequest((request, response) => {
+  let data;
+  let status;
+
+  if (request.body.launch === true || request.body.launch === false) {
     switch (request.get('content-type')) {
       case 'application/json':
         data = request.body;
