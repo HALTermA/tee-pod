a26ce3950bdf3d3493dcaaa148365c4ffe98cc50
Micchili <wot.dannbo@gmail.com>
2020-07-06 12:20:02 +0900

    firestoreにデータを入れるよう処理を変更

--- functions/index.js
+++ functions/index.js
@@ -1,82 +1,103 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin")
+const servisAcount = require("../tee-pod-firebase-adminsdk-p6k1p-eb0cbe3787.json")
+// C:\Users\user\Desktop\curl-7.70.0-win64-mingw\bin\curl.exe -X POST -H "Content-Type:application/json" -d @light.json http://localhost:5000/tee-pod/us-central1/changeColor
+admin.initializeApp({
+  credential: admin.credential.cert(servisAcount),
+  databaseURL: "https://tee-pod.firebaseio.com"
+})
 
-admin.initializeApp(functions.config().firebase)
 const fireStore = admin.firestore()
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+exports.hello = functions.https.onRequest((request, response) => {
+  var createRef = fireStore.collection('hello');
+  createRef.doc('sample').set({lanch: true, color: 'normal' })
+  response.status(200).send("deketa")
+})
 
-exports.returnColor = functions.https.onRequest((request, response) => {
+exports.changeColor = functions.https.onRequest((request, response) => {
   const normal = "normal", warning = "warning", partyPeople = "paetyPeople";
-  let data;
-  let status;
-  const citiesRef = fireStore.collection('tee_pod').doc('status');
+  const changeColorRef = fireStore.collection('tee-pod').doc('status');
+  console.log(Boolean(request.body.color.colors.warning === normal))
 
-  if (request.body.color.colors === normal || warning || partyPeople ) {
-    switch (request.get('content-type')) {
-      case 'application/json':
-        data = request.body.color.colors;
-        status = 200;
-        citiesRef.set({
-          color: data
+  if (Object.keys(request.body).length) {
+    if (request.get('content-type') === 'application/json') {
+      if (request.body.color.colors.normal === normal) {
+        changeColorRef.update({
+          color: request.body.color.colors.normal
         })
-        break;
-      default:
-        data = "Context-Typeを設定してください";
-        status = 400;
-        break;
+        .catch(err => response.status.send("データベース側でエラーが起きました", err));
+        response.status(200).send("設定を変更しました");
+      }
+      else if (request.body.color.colors.warning === warning) {
+        changeColorRef.update({
+          color: request.body.color.colors.warning
+        })
+        .catch(err => response.status.send("データベース側でエラーが起きました", err));
+        response.status(200).send("設定を変更しました");
+      }
+      else if (request.body.color.colors.partyPeople === partyPeople) {
+        changeColorRef.update({
+          color: request.body.color.colors.partyPeople
+        })
+        .catch(err => response.status(400).send("データベース側でエラーが起きました", err));
+        response.status(200).send("設定を変更しました");
+      }
+      else {
+        response.status(400).send("JSONの値が違います");
+      }
     }
-    response.status(status).send("設定が更新されました");
+    else {
+      response.status(400).send("Context-Typeをapplication/jsonに設定してください");
+    }
   }
   else {
-    response.status(400).send("JSONの値が違います");
+    response.status(400).send("JSONをください");
   }
 });
 
 exports.isLight = functions.https.onRequest((request, response) => {
-  let data;
+  var data;
   let status;
+  const lightRef = fireStore.collection('tee-pod').doc('status');
 
-  if (request.body.launch === true || request.body.launch === false) {
-    switch (request.get('content-type')) {
-      case 'application/json':
-        data = request.body;
-        status = 200;
-        break;
-      default:
-        data = "Context-Typeを設定してください";
-        status = 400;
-        break;
+  if (Object.keys(request.body).length) {
+    if (request.body.launch === true || request.body.launch === false) {
+      switch (request.get('content-type')) {
+        case 'application/json':
+          data = "設定が更新されました";
+          status = 200;
+          lightRef.update({
+            launch: request.body.launch
+          })
+          .catch(err => data = "データベース側でエラーが起きました" + err);
+          break;
+        default:
+          data = "Context-Typeを設定してください";
+          status = 400;
+          break;
+      }
+      response.status(status).send(data);
     }
-    response.status(status).send(data);
+    else {
+      response.status(400).send("JSONの値が違います");
+    }
   }
-  else {
-    response.status(400).send("JSONの値が違います");
+  else{
+    response.status(400).send("JSONをください");
   }
 });
 
 
 exports.status = functions.https.onRequest((request, response) => {
-  let data;
-  let httpStatus;
-  const citiesRef = fireStore.collection('tee_pod');
+  const statusRef = fireStore.collection('tee-pod').doc('status');
 
-  citiesRef.doc('status')
-  .set({
-    color: 'San Francisco', state: 'CA', country: 'USA',
-    capital: false, population: 860000 })
-
-  const cityRef = fireStore.collection('tee_pod').doc('status')
-  cityRef.get()
-  .then(doc => {
-    if (!doc.exists) {
-      response.send('No such document!')
-    } else {
-      response.send(doc.data())
-      }
-    })
-    .catch(err => {
-      response.send('not found')
-    })
+  if (request.method === "POST") {
+    statusRef.get()
+      .then(doc => !doc.exists ? response.send('No such document!') : response.send(doc.data()))
+      .catch(err => response.send(err));
+  }
+  else {
+    response.status(400).send("POSTで通信してください");
+  }
 })
